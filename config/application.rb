f627a17cf6d3107c7934c177144c0e2cb24bf3e8
katanoZ <katano9@yahoo.co.jp>
2019-04-14 10:34:41 +0900

    [change]東京のタイムゾーンで表示されるよう変更

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ Bundler.require(*Rails.groups)
 module Funding
   class Application < Rails::Application
     config.load_defaults 5.2
-
+    config.time_zone = 'Asia/Tokyo'
+    
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
