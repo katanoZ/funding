a06aa0deae63d2ec60bb6667394902a7261a54f3
katanoZ <katano9@yahoo.co.jp>
2019-04-18 08:37:36 +0900

    [clean]レビュー指摘対応:projectモデルのscopeテスト修正

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,46 +2,94 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   let(:user) { create(:user) }
-  let(:guest) { create(:user) }
-
-  # 検証対象のプロジェクト
-  let!(:owner_project_not_invested) { create(:project, owner: user) }
-  let!(:owner_project_invested_by_guest) { create(:project, owner: user) }
-  let!(:not_owner_project_not_invested) { create(:project) }
-  let!(:not_owner_project_invested_by_guest) { create(:project) }
-  let!(:not_owner_project_invested_by_user) { create(:project) }
-
-  before do
-    # プロジェクトに出資する
-    create(:investment, project: owner_project_invested_by_guest, user: guest)
-    create(:investment, project: not_owner_project_invested_by_guest, user: guest)
-    create(:investment, project: not_owner_project_invested_by_user, user: user)
-  end
 
   describe '.investable' do
-    it 'ユーザがオーナーではない、かつ、ユーザーが出資していないプロジェクトが返ること' do
-      expect(Project.investable(user))
-        .to contain_exactly not_owner_project_not_invested,
-                            not_owner_project_invested_by_guest
+    context '該当のデータが存在する場合' do
+      let(:project) { create(:project) }
+      let(:results) { Project.investable(user) }
+
+      before do
+        create(:investment, project: project)
+      end
+
+      it '件数が正しいこと' do
+        expect(results.count).to eq 1
+      end
+
+      it '内容が正しいこと' do
+        expect(user.owner?(results.first)).to be_falsey
+        expect(user.invest_in?(results.first)).to be_falsey
+      end
+    end
+
+    context '該当のデータが存在しない場合' do
+      let(:project) { create(:project) }
+      let(:results) { Project.investable(user) }
+
+      before do
+        create(:project, owner: user)
+        create(:investment, project: project, user: user)
+      end
+
+      it '件数が正しいこと' do
+        expect(results.count).to eq 0
+      end
     end
   end
 
-  describe '.not_owned_by' do
-    it 'ユーザがオーナーではないプロジェクトが返ること' do
-      expect(Project.not_owned_by(user))
-        .to contain_exactly not_owner_project_not_invested,
-                            not_owner_project_invested_by_guest,
-                            not_owner_project_invested_by_user
+  describe '.not_owned_by' do 
+    context '該当のデータが存在する場合' do
+      let!(:project) { create(:project) }
+      let!(:results) { Project.not_owned_by(user) }
+
+      it '件数が正しいこと' do
+        expect(results.count).to eq 1
+      end
+
+      it '内容が正しいこと' do
+        expect(user.owner?(results.first)).to be_falsey
+      end
+    end
+
+    context '該当のデータが存在しない場合' do
+      let!(:project) { create(:project, owner: user) }
+      let!(:results) { Project.not_owned_by(user) }
+
+      it '件数が正しいこと' do
+        expect(results.count).to eq 0
+      end
     end
   end
 
   describe '.not_invested_by' do
-    it 'ユーザが出資していないプロジェクトが返ること' do
-      expect(Project.not_invested_by(user))
-        .to contain_exactly owner_project_not_invested,
-                            owner_project_invested_by_guest,
-                            not_owner_project_not_invested,
-                            not_owner_project_invested_by_guest
+    context '該当のデータが存在する場合' do
+      let(:project) { create(:project) }
+      let(:results) { Project.not_invested_by(user) }
+
+      before do
+        create(:investment, project: project)
+      end
+
+      it '件数が正しいこと' do
+        expect(results.count).to eq 1
+      end
+
+      it '内容が正しいこと' do
+        expect(user.invest_in?(results.first)).to be_falsey
+      end
+    end
+
+    context '該当のデータが存在しない場合' do
+      let(:project) { create(:project) }
+      let(:results) { Project.not_invested_by(user) }
+
+      before do
+        create(:investment, project: project, user: user)
+      end
+
+      it '件数が正しいこと' do
+        expect(results.count).to eq 0
+      end
     end
   end
 end
