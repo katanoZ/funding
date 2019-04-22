8adca30b7d2a272380207e25f02e2aed3dbe0e61
katanoZ <katano9@yahoo.co.jp>
2019-04-23 08:32:15 +0900

    [add]全ユーザーの取引状況が見れるページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :administer do
+    get 'investments/index'
+  end
   devise_for :users
 
   root 'projects#index'
@@ -11,5 +14,11 @@ Rails.application.routes.draw do
   namespace :admin do
     root 'projects#index'
     resources :projects
+  end
+
+  # 運営管理車画面
+  namespace :administrator do
+    root 'investments#index'
+    resources :investments, only: %i[index]
   end
 end
