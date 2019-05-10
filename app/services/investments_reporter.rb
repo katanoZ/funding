081f6b526d74a2f2fe7717a41e540f6c5fe98eee
katanoZ <katano9@yahoo.co.jp>
2019-05-10 16:18:02 +0900

    [clean]investmrnts_reporterのメソッドを整理

--- app/services/investments_reporter.rb
+++ app/services/investments_reporter.rb
@@ -5,7 +5,6 @@ class InvestmentsReporter
   end
 
   def execute
-    range = (form.start_date.beginning_of_day)..(form.end_date.end_of_day)
     Investment.for_projects_created_by(user)
               .created_within(range)
               .includes(:project, :user)
@@ -15,4 +14,8 @@ class InvestmentsReporter
   private
 
   attr_reader :form, :user
+
+  def range
+    (form.start_date.beginning_of_day)..(form.end_date.end_of_day)
+  end
 end
