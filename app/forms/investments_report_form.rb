067eb1507bd0a6858eeabcd4c8c6830c8ca5ee7d
katanoZ <katano9@yahoo.co.jp>
2019-05-14 15:09:16 +0900

    [update]投資状況レポートフォームのバリデーションを修正
    未来日を入力させないバリデーションは、サービスのバリデーションとして不適当なため削除
    未来の日付は論理的には妥当な入力値であり、未来の日付を入力しても該当のデータがないだけなので、バリデーションとしては扱わない

--- app/forms/investments_report_form.rb
+++ app/forms/investments_report_form.rb
@@ -8,27 +8,14 @@ class InvestmentsReportForm
   validates :start_date, presence: true
   validates :end_date, presence: true
 
-  validate :verify_term
-  validate :verify_range, if: :start_and_end_dates_entered?
+  validate :verify_range
 
   private
 
-  def verify_term
-    verify_date_term(start_date, :start_date) if start_date.present?
-    verify_date_term(end_date, :end_date) if end_date.present?
-  end
-
-  def verify_date_term(date, attribute)
-    return if date.between?(Date.today.prev_year, Date.today)
-
-    errors.add(attribute, 'は1年前から本日までの期間で入力してください')
-  end
-
-  def start_and_end_dates_entered?
-    start_date && end_date
-  end
-
   def verify_range
-    errors.add(:base, '開始日は終了日より前に設定してください') if start_date > end_date
+    return if start_date.nil? || end_date.nil?
+    return if start_date <= end_date
+
+    errors.add(:base, '開始日は終了日より前に設定してください')
   end
 end
